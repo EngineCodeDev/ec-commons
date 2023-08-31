cad86267e8bd62e273df46ca5befb7c8a2002794
tom-zoladz <tomasz.d.zoladz@gmail.com>
2023-08-31 17:07:56 +0200

    Spring boot project initialization #1

---
+++ src/test/java/dev/enginecode/eccommons/EcCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.enginecode.eccommons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcCommonsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
