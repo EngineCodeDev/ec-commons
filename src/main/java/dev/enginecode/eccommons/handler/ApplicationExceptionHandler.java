15de92d85a96d539547ec15a335c50ffa958ae91
michalrys <michalrys@gmail.com>
2023-11-15 20:40:46 +0100

    refactore -remove handler for SQLException because it is never being thrown

--- src/main/java/dev/enginecode/eccommons/handler/ApplicationExceptionHandler.java
+++ src/main/java/dev/enginecode/eccommons/handler/ApplicationExceptionHandler.java
@@ -1,23 +1,12 @@
 package dev.enginecode.eccommons.handler;
 
 import dev.enginecode.eccommons.exception.EngineCodeException;
-import dev.enginecode.eccommons.exception.EngineCodeExceptionGroup;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.sql.SQLException;
-
 @RestControllerAdvice
 public class ApplicationExceptionHandler {
-    @ExceptionHandler(SQLException.class)
-    public ResponseEntity<ErrorResponse> handle(SQLException exception) {
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(new ErrorResponse(EngineCodeExceptionGroup.INFRASTRUCTURE_ERROR.get(), exception.getMessage()));
-    }
-
     @ExceptionHandler(EngineCodeException.class)
     public ResponseEntity<ErrorResponse> handle(EngineCodeException exception) {
         return ResponseEntity
