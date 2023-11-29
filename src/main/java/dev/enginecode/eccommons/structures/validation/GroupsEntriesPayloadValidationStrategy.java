7a99e02511b5f77ee30728e908b032a2aec7a7b8
tom-zoladz <tomasz.d.zoladz@gmail.com>
2023-11-29 20:38:54 +0100

    GroupsEntriesPayloadValidationStrategy added

---
+++ src/main/java/dev/enginecode/eccommons/structures/validation/GroupsEntriesPayloadValidationStrategy.java
@@ -0,0 +1,25 @@
+package dev.enginecode.eccommons.structures.validation;
+
+import dev.enginecode.eccommons.structures.model.DataModel;
+import org.springframework.stereotype.Component;
+
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class GroupsEntriesPayloadValidationStrategy implements EntriesPayloadValidationStrategy {
+    @Override
+    public List<ValidationError> validate(EntriesPayload payload, DataModel dataModel) {
+        LinkedHashSet<String> unknownGroups = payload.groups().stream()
+                .filter(group -> !dataModel.groupContents().containsKey(group))
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+
+        return unknownGroups.isEmpty()
+                ? List.of()
+                : List.of(new ValidationError(
+                        "groups","UNKNOWN_GROUPS",unknownGroups + " not found"
+        ));
+    }
+
+}
